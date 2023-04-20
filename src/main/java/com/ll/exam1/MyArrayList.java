8127ac3b8a6b3920ba28252b8aa63486c944bdf5
wnstn7671 <wnstn7671@naver.com>
2023-04-20 21:53:48 +0900

    리스트구현까지완료

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -1,15 +1,26 @@
 package com.ll.exam1;
 
+import java.util.stream.IntStream;
+
 public class MyArrayList<T> {
-    private String[] data = new String[2];
+    public boolean debug = false;
+    private Object[] data;
 
     private int size = 0;
+
+    public MyArrayList() {
+        this(2);
+    }
+
+    public MyArrayList(int dataLength) {
+        data = new Object[dataLength];
+    }
 
     public int size() {
         return size;
     }
 
-    public boolean add(String element) {
+    public boolean add(T element) {
         // 만약에 공간이 부족하면 새 data 객체를 만든다.
         makeNewDataIfNotEnough();
         data[size] = element;
@@ -28,11 +39,15 @@ public class MyArrayList<T> {
 
     private void makeNewData() {
         // 새 배열을 만든다.(새 업체를 만든다.)
-        String[] newData = new String[data.length * 2];
+        Object[] newData = new Object[data.length * 2];
 
         // 기존 창고에 있던 물품들을 전부 새 창고로 옮긴다.
-        for ( int i = 0; i < data.length; i++ ) {
+        for (int i = 0; i < data.length; i++) {
             newData[i] = data[i];
+        }
+
+        if (debug) {
+            System.out.printf("배열크기 증가 : %d => %d\n", data.length, newData.length);
         }
 
         // 기존 창고과 계약을 해지한다.
@@ -44,7 +59,68 @@ public class MyArrayList<T> {
         return size >= data.length;
     }
 
-    public String get(int index) {
-        return data[index];
+    public T get(int index) {
+        return (T) data[index];
+    }
+
+    public int indexOf(T element) {
+//        for (int i = 0; i < data.length; i++)
+//        {
+//            if (element.equals(data[i])) return i;
+//        }
+//
+//        return -1;
+
+        // 더 깔끔한 버전
+        return IntStream.range(0, size)
+                .filter(index -> element.equals(data[index]))
+                .findFirst()
+                .orElse(-1);
+
+        // 복잡한 연산까지 할 수 있는 버전
+        // 예를들어서 index와 element 에 대한 복합조건을 filter에 사용가능
+    }
+    public boolean contains(T element) {
+        return indexOf(element) != -1;
+    }
+    public boolean add(int index, T element) {
+        // 만약에 공간이 부족하면 새 data 객체를 만든다.
+        makeNewDataIfNotEnough();
+
+        // 해당 공간을 빈공간으로 만든다.
+        makeEmptyIndex(index);
+
+        data[index] = element;
+
+        size++;
+
+        return true;
+    }
+
+    private void makeEmptyIndex(int index) {
+        // 맨뒤 승객부터 뒤로 1칸씩 이동
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+    }
+    public T set(int index, T element) {
+        T old = (T) data[index];
+
+        data[index] = element;
+
+        return old;
+    }
+    public T remove(int index) {
+        T old = (T) data[index];
+
+        // 앞에서 부터 자리 이동
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+
+        size--;
+
+        return old;
     }
 }
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -2,21 +2,26 @@ package com.ll.exam1;
 
 import com.ll.TestUt;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class MyArrayListTest {
     @Test
     @DisplayName("size()")
-    void t1() {
+    void t01() {
         MyArrayList<String> list = new MyArrayList<>();
         assertThat(list.size()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("add(\"사과\")")
-    void t2() {
+    void t02() {
         MyArrayList<String> list = new MyArrayList<>();
 
         list.add("사과");
@@ -27,35 +32,165 @@ public class MyArrayListTest {
 
     @Test
     @DisplayName("get(1)")
-    void t3() {
+    void t03() {
         MyArrayList<String> list = new MyArrayList<>();
-
-        list.add("사과"); // data[0];
-        list.add("포도"); // data[1];
-
-        assertThat(list.get(0)).isEqualTo("사과"); // data[0];
-        assertThat(list.get(1)).isEqualTo("포도"); // data[1];
-    }
-
-    @Test
-    @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
-    void t4() {
-        MyArrayList<String> list = new MyArrayList<>();
-
-        String[] data = TestUt.getFieldValue(list, "data", null);
-        assertThat(data.length).isEqualTo(2);
 
         list.add("사과");
         list.add("포도");
 
-        data = TestUt.getFieldValue(list, "data", null);
-        assertThat(data.length).isEqualTo(2);
+        String e0 = list.get(0);
+        String e1 = list.get(1);
 
-        list.add("당근");
+        assertThat(e0).isEqualTo("사과");
+        assertThat(e1).isEqualTo("포도");
+    }
 
-        data = TestUt.getFieldValue(list, "data", null);
-        assertThat(data.length).isEqualTo(4);
+    @Test
+    @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
+    void t04() {
+        MyArrayList<String> list = new MyArrayList<>();
 
+        // 초창기 배열의 길이
+        int dataLength1 = ((Object[]) TestUt.getFieldValue(list, "data", null)).length;
+
+        // IntStream.range(0, 10); = [0, ... 9] 까지의 int 스트림 발생
+        // 딱 1번 넘칠만큼의 데이터를 넣는다.
+        IntStream.range(0, dataLength1 + 1)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+
+        // 현재 배열의 길이
+        int dataLength2 = ((Object[]) TestUt.getFieldValue(list, "data", null)).length;
+        assertThat(dataLength2).isGreaterThan(dataLength1);
+    }
+
+    @Test
+    @DisplayName("MyArrayList 초기화(new) 시 생성자 인자로 data 배열의 사이즈 지정")
+    void t05() {
+        MyArrayList<String> list = new MyArrayList<>(200);
+
+        // 초창기 배열의 길이
+        int dataLength = ((Object[]) TestUt.getFieldValue(list, "data", null)).length;
+
+        assertThat(dataLength).isEqualTo(200);
+    }
+
+    @Test
+    @DisplayName("디버그 : 데이터를 많이 넣으면 배열증가가 많이 발생함")
+    void t06() {
+        MyArrayList<String> list = new MyArrayList<>();
+        list.debug = true;
+
+        IntStream.range(0, 100)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+    }
+
+    @Test
+    @DisplayName("디버그 : 애초에 큰 배열로 만들면 데이터를 많이 넣어도 배열증가가 빈번하지 않음")
+    void t07() {
+        MyArrayList<String> list = new MyArrayList<>(25);
+        list.debug = true;
+
+        IntStream.range(0, 100)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+    }
+
+    @Test
+    @DisplayName("indexOf")
+    void t08() {
+        MyArrayList<String> list = new MyArrayList<>(100);
+
+        IntStream.range(0, 100)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+
+        assertThat(list.indexOf("사과 0")).isEqualTo(0);
+        assertThat(list.indexOf("사과 1")).isEqualTo(1);
+        assertThat(list.indexOf("사과 5")).isEqualTo(5);
+        assertThat(list.indexOf("사과 99")).isEqualTo(99);
+        assertThat(list.indexOf("사과 100")).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("add(true)")
+    void t09() {
+        MyArrayList<Boolean> list = new MyArrayList<>();
+
+        list.add(true);
+        list.add(false);
+
+        assertThat(list.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("get(1)")
+    void t10() {
+        MyArrayList<Boolean> list = new MyArrayList<>();
+
+        list.add(true);
+        list.add(false);
+
+        boolean e0 = list.get(0);
+        boolean e1 = list.get(1);
+
+        assertThat(e0).isEqualTo(true);
+        assertThat(e1).isEqualTo(false);
+    }
+    @Test
+    @DisplayName("contains(\"사과 0\")")
+    void t11() {
+        MyArrayList<String> list = new MyArrayList<>(2);
+
+        IntStream.range(0, 2)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+
+        assertThat(list.contains("사과 0")).isEqualTo(true);
+        assertThat(list.contains("사과 1")).isEqualTo(true);
+        assertThat(list.contains("사과 2")).isEqualTo(false);
+    }
+    @Test
+    @DisplayName("addAt")
+    void t12() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("Element1"); // 0번 좌석에 착석
+        list.add("Element2"); // 1번 좌석에 착석
+        list.add("Element3"); // 2번 좌석에 착석
+
+        list.add(1, "Element4");
+        // 2 => 3
+        // 1 => 2
+        // 1번 좌석에 착석
+
+        assertThat(list.size()).isEqualTo(4);
+
+        assertThat(list.get(0)).isEqualTo("Element1");
+        assertThat(list.get(1)).isEqualTo("Element4");
+        assertThat(list.get(2)).isEqualTo("Element2");
+        assertThat(list.get(3)).isEqualTo("Element3");
+    }
+    @Test
+    @DisplayName("set")
+    void t13() {
+        MyArrayList<String> list = new MyArrayList<>();
+        list.add("Element1"); // 0
+        list.add("Element2"); // 1
+        list.add("Element3"); // 2
+
+        assertThat(list.set(0, "Element4")).isEqualTo("Element1");
         assertThat(list.size()).isEqualTo(3);
+
+        assertThat(list.get(0)).isEqualTo("Element4");
+        assertThat(list.get(1)).isEqualTo("Element2");
+        assertThat(list.get(2)).isEqualTo("Element3");
+    }
+    @Test
+    @DisplayName("remove")
+    void t14() {
+        MyArrayList<String> list = new MyArrayList<>();
+        list.add("Element1");
+        list.add("Element2");
+        list.add("Element3");
+
+        assertThat(list.remove(0)).isEqualTo("Element1");
+        assertThat(list.size()).isEqualTo(2);
     }
 }
